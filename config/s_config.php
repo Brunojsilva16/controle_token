54aef86b5cc0a902a1e01e83c7aa22a53797b783
Brunojsilva16 <49538831+Brunojsilva16@users.noreply.github.com>
2025-10-20 06:13:39 -0300

    Add files via upload

---
+++ config/s_config.php
@@ -0,0 +1,39 @@
+<?php
+/* Configurações básicas do site */
+$titleSite = "ASSISTA ";// nome do site
+$descSite = "ASSISTA"; // descrição do site
+$autor = "Bruno Silva";// desenvolvedor do site
+$compania = "ASSISTA";
+
+$metaCharset = "utf-8"; // codificação do servidor (PADRÃO UTF-8)
+
+/* Configurações de mídias digitais e contato */
+$instUrl = "https://www.instagram.com/assista_clinica/";
+$faceUrl = "#";
+$twitterUrl = "#";
+$whatsApp = "#";
+$whatsAppApi = "https://web.whatsapp.com/send?phone=5581995343141";
+
+$telefone = "(81) 3241 3141";
+$celular = "(81) 99534 3141";
+$webmailUrl = "";
+$contatoEmail = "contato@clinicaassista.com.br";
+$copyRight_title = "ASSISTA";
+$copyRight_text = "Todos direitos reservados";
+$copyRight_ano = "© 2023";
+
+/* Configurações de Endereço */
+$endereco = "Rua José Nunes da Cunha, 5665 - Candeias";
+$referenca = "#";
+$endereco2 = "Jaboatão dos Guararapes/PE - CEP: 54440-030";
+$mapa = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3948.980954339291!2d-34.9181743!3d-8.204668899999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7aae13cb7d63577%3A0xb14db37309770a75!2sR.%20Jos%C3%A9%20Nunes%20da%20Cunha%2C%205665%20-%20Candeias%2C%20Jaboat%C3%A3o%20dos%20Guararapes%20-%20PE%2C%2054440-030!5e0!3m2!1spt-BR!2sbr!4v1674749146763!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+
+$mapaLink = 'https://goo.gl/maps/xJANybrmEhXMtmr76';
+
+/* Preferências de Buscador */
+$linguagemsite = "portuguese"; // idioma do site
+$distribuicaosite = "Global"; // O site deverá ser exibido em pesquisas do mundo todo
+$classificacaoservicos = "Organização"; // classificação dos serviços ou produtos
+$sitekeys = "recife, olinda, jaboatão dos guararapes, pernambuco, brasil"; // palavras chave
+$robots = "index, follow"; // index = indexar site nos buscadores | follow = permite os buscadores a acompanharem as páginas
+?>
